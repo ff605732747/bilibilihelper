7a9eb6e2b203959b03b1866ddfc3c6ba3dd92e2f
YUU <fwq605732747@live.cn>
2023-07-06 14:01:17 +0800

    123456

--- src/bilibili.py
+++ src/bilibili.py
@@ -50,7 +50,7 @@ class BiliBili:
     }
 
     def __init__(self, **config) -> None:
-        self.cookie = os.environ.get('cookie')
+        self.cookie = os.environ.get('COOKIE')
         self.options = config.get("options", {})
 
         self.sid = BiliBili.extract("sid", self.cookie)
